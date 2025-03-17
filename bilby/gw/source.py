65e7fea82540f8fbc72337d23e2ec4d0804796ff
Harsh Narola <hnarola@stbc-i3.nikhef.nl>
2025-03-17 18:32:16 +0100

    fist commit for waveform systematics

--- bilby/gw/source.py
+++ bilby/gw/source.py
@@ -9,12 +9,166 @@ from .utils import (lalsim_GetApproximantFromString,
                     lalsim_SimInspiralChooseFDWaveformSequence,
                     safe_cast_mode_to_int)
 
+from scipy.interpolate import CubicSpline
+
+
 UNUSED_KWARGS_MESSAGE = """There are unused waveform kwargs. This is deprecated behavior and will
 result in an error in future releases. Make sure all of the waveform kwargs are correctly
 spelled.
 
 Unused waveform_kwargs: {waveform_kwargs}
 """
+
+def lal_binary_black_hole_with_amplitude_phase_modification(
+        frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, **kwargs):
+    """ A Binary Black Hole waveform model using lalsimulation
+
+    Parameters
+    ==========
+    frequency_array: array_like
+        The frequencies at which we want to calculate the strain
+    mass_1: float
+        The mass of the heavier object in solar masses
+    mass_2: float
+        The mass of the lighter object in solar masses
+    luminosity_distance: float
+        The luminosity distance in megaparsec
+    a_1: float
+        Dimensionless primary spin magnitude
+    tilt_1: float
+        Primary tilt angle
+    phi_12: float
+        Azimuthal angle between the two component spins
+    a_2: float
+        Dimensionless secondary spin magnitude
+    tilt_2: float
+        Secondary tilt angle
+    phi_jl: float
+        Azimuthal angle between the total binary angular momentum and the
+        orbital angular momentum
+    theta_jn: float
+        Angle between the total binary angular momentum and the line of sight
+    phase: float
+        The phase at reference frequency or peak amplitude (depends on waveform)
+    kwargs: dict
+        Optional keyword arguments
+        Supported arguments:
+
+        - waveform_approximant
+        - reference_frequency
+        - minimum_frequency
+        - maximum_frequency
+        - catch_waveform_errors
+        - pn_spin_order
+        - pn_tidal_order
+        - pn_phase_order
+        - pn_amplitude_order
+        - mode_array:
+          Activate a specific mode array and evaluate the model using those
+          modes only.  e.g. waveform_arguments =
+          dict(waveform_approximant='IMRPhenomHM', mode_array=[[2,2],[2,-2])
+          returns the 22 and 2-2 modes only of IMRPhenomHM.  You can only
+          specify modes that are included in that particular model.  e.g.
+          waveform_arguments = dict(waveform_approximant='IMRPhenomHM',
+          mode_array=[[2,2],[2,-2],[5,5],[5,-5]]) is not allowed because the
+          55 modes are not included in this model.  Be aware that some models
+          only take positive modes and return the positive and the negative
+          mode together, while others need to call both.  e.g.
+          waveform_arguments = dict(waveform_approximant='IMRPhenomHM',
+          mode_array=[[2,2],[4,-4]]) returns the 22 and 2-2 of IMRPhenomHM.
+          However, waveform_arguments =
+          dict(waveform_approximant='IMRPhenomXHM', mode_array=[[2,2],[4,-4]])
+          returns the 22 and 4-4 of IMRPhenomXHM.
+        - lal_waveform_dictionary:
+          A dictionary (lal.Dict) of arguments passed to the lalsimulation
+          waveform generator. The arguments are specific to the waveform used.
+
+    Returns
+    =======
+    dict: A dictionary with the plus and cross polarisation strain modes
+    """
+    waveform_kwargs = dict(
+        waveform_approximant='IMRPhenomPv2', reference_frequency=50.0,
+        minimum_frequency=20.0, maximum_frequency=frequency_array[-1],
+        catch_waveform_errors=False, pn_spin_order=-1, pn_tidal_order=-1,
+        pn_phase_order=-1, pn_amplitude_order=0)
+    waveform_kwargs.update(kwargs)
+    polas = _base_lal_cbc_fd_waveform(
+        frequency_array=frequency_array, mass_1=mass_1, mass_2=mass_2,
+        luminosity_distance=luminosity_distance, theta_jn=theta_jn, phase=phase,
+        a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+        phi_jl=phi_jl, **waveform_kwargs)
+    
+
+    phase0 = {}
+    for pol in polas:
+        phase0[pol] = np.unwrap(np.angle(polas[pol]))
+
+    if waveform_kwargs['error_in_phase'] not in ['relative', 'absolute']:
+        raise ValueError('Only two types of errors are supported, "relative" and "absolute".')
+    
+    if waveform_kwargs['modification_type'] == 'cubic_spline':
+        wf_nodal_points = waveform_kwargs['nodal_points']
+        delta_amplitude_arr = waveform_kwargs['delta_amplitude']
+        delta_phase_arr = waveform_kwargs['delta_phase']
+        delta_amplitude_interp = CubicSpline(wf_nodal_points, delta_amplitude_arr)
+        delta_phase_interp = CubicSpline(wf_nodal_points, delta_phase_arr)
+
+
+        if waveform_kwargs['error_in_phase'] == 'relative':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude_interp(frequency_array)) * np.exp(1j * phase0[pol] * delta_phase_interp(frequency_array))
+
+        elif waveform_kwargs['error_in_phase'] == 'absolute':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude_interp(frequency_array)) * np.exp(1j * delta_phase_interp(frequency_array))
+            
+
+    elif waveform_kwargs['modification_type'] == 'constant_shift':
+        delta_amplitude = waveform_kwargs['delta_amplitude']
+        delta_phase = waveform_kwargs['delta_phase']
+        
+        if waveform_kwargs['error_in_phase'] == 'relative':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude) * np.exp(1j * phase0[pol] * delta_phase)
+
+        elif waveform_kwargs['error_in_phase'] == 'absolute':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude) * np.exp(1j * delta_phase)
+
+
+
+    elif waveform_kwargs['modification_type'] == 'cubic_spline_nodes':
+        f_lower = waveform_kwargs['minimum_frequency']
+        f_high_wferror = waveform_kwargs['f_high_wferror']
+        n_nodes_wferror = int(waveform_kwargs['n_nodes_wferror'])
+        wf_nodal_points = np.logspace(np.log10(f_lower), np.log10(f_high_wferror), n_nodes_wferror)
+
+        delta_amplitude_arr = np.hstack([waveform_kwargs['wferror_amplitude_{}'.format(i)] for i in range(len(wf_nodal_points))])
+        delta_phase_arr = np.hstack([waveform_kwargs['wferror_phase_{}'.format(i)]for i in range(len(wf_nodal_points))])
+
+        delta_amplitude_interp = CubicSpline(wf_nodal_points, delta_amplitude_arr)
+        delta_phase_interp = CubicSpline(wf_nodal_points, delta_phase_arr)
+
+        if waveform_kwargs['error_in_phase'] == 'relative':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude_interp) * np.exp(1j * phase0[pol] * delta_phase_interp)
+
+        elif waveform_kwargs['error_in_phase'] == 'absolute':
+            for pol in polas:
+                polas[pol] *= (1+delta_amplitude_interp) * np.exp(1j * delta_phase_interp)
+
+        
+        
+
+    else:
+        raise TypeError("Currently: only two types of modification are supported")
+
+    return polas
+
+
+
 
 
 def gwsignal_binary_black_hole(frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
